32e266c8c33a233d22f1a2996e1f399a34e9f97a
Sami-Wafula <samiwafula3257@gmail.com>
2023-01-19 10:12:01 +0300

    Revert "add methods"
    This reverts commit e52466ea849a990dc32c51bf723bebe5b1a3487c.

--- src/components/form-component/Signup.js
+++ src/components/form-component/Signup.js
@@ -12,20 +12,6 @@ export default class Signup extends Component {
         age: ''
     }
 
-    previousStep = () => {
-        const { step } = this.state;
-        this.setState({ step: step - 1})
-    }
-
-    nextStep = () => {
-        const { step } = this.state;
-        this.setState({ step: step + 1 })
-    }
-
-    handleChange = input => e => {
-        this.setState({ [input]: e.target.value })
-    }
-
     render() {
         return (
             <div>
