c37a57369a8285c587fe157764f47eb40daf79a8
Sami-Wafula <samiwafula3257@gmail.com>
2023-01-19 11:17:28 +0300

    add forms

--- src/components/form-component/OtherInformation.js
+++ src/components/form-component/OtherInformation.js
@@ -3,6 +3,32 @@ import React from "react";
 const OtherInformation = ({ nextStep, handleChange, values }) => {
     return (
         <>
+        <form>
+            <label>Country
+                <input 
+                type="text" 
+                placeholder="country" 
+                value={values.country} 
+                onChange={handleChange('country')}
+                />
+            </label>
+            <label>Sex
+                <input 
+                type="text" 
+                placeholder="sex" 
+                value={values.sex} 
+                onChange={handleChange('sex')}
+                />
+            </label>
+            <label>Age
+                <input 
+                type="number" 
+                placeholder="age" 
+                value={values.age} 
+                onChange={handleChange('age')}
+                />
+            </label>
+        </form>
         </>
     )
 }
--- src/components/form-component/UserInformation.js
+++ src/components/form-component/UserInformation.js
@@ -3,6 +3,32 @@ import React from "react";
 const UserInformation = ({ nextStep, handleChange, values }) => {
     return (
         <>
+        <form>
+            <label>Username
+                <input 
+                type="text" 
+                placeholder="username" 
+                value={values.username} 
+                onChange={handleChange('username')}
+                />
+            </label>
+            <label>Email
+                <input 
+                type="text" 
+                placeholder="email" 
+                value={values.email} 
+                onChange={handleChange('email')}
+                />
+            </label>
+            <label>Password
+                <input 
+                type="text" 
+                placeholder="password" 
+                value={values.password} 
+                onChange={handleChange('password')}
+                />
+            </label>
+        </form>
         </>
     )
 }
