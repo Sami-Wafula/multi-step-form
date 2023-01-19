f515158b5f5b9ee82ada4c5657f084f5c0a900bd
Sami-Wafula <samiwafula3257@gmail.com>
2023-01-19 10:10:21 +0300

    Revert "destructuring"
    This reverts commit da6455660e273d562f4d5529770faf635d293b49.
    
    wrong commit

--- src/components/form-component/Signup.js
+++ src/components/form-component/Signup.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 export default class Signup extends Component {
 
+    state = {
+        step: 1,
+        username: '',
+        email: '',
+        password: '',
+        country: '',
+        sex: '',
+        age: ''
+    }
+
     previousStep = () => {
         const { step } = this.state;
         this.setState({ step: step - 1})
@@ -17,12 +27,6 @@ export default class Signup extends Component {
     }
 
     render() {
-        const { step } = this.state;
-    
-        const { username, email, password, country, sex, age } = this.state;
-
-        const values = { username, email, password, country, sex, age }
-        
         return (
             <div>
                 <h1>Welcome, please provide some information to get started</h1>
